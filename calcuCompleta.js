ed2a994f795c379bef177220582c51ae2ab0c628
JonathanChallco <jonathanchallco21@gmail.com>
2021-09-12 20:49:48 -0300

    Agregador calculadora completa

---
+++ calcuCompleta.js
@@ -0,0 +1,5 @@
+const dividir = require("./dividir")
+const multiplicar = require("./multiplicar")
+const resta = require("./Operadores/resta")
+const suma = require("./Operadores/suma")
+console.log(multiplicar(5,2));
